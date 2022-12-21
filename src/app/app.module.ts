dc674f59fd73b4d127222b95f0b3f8997860055e
megana <meghanadumpeti17@gmail.com>
2022-12-21 19:46:24 +0530

    msg

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { GameComponent } from './game/game.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoardComponent,
+    GameComponent,
+  ],
+  imports: [
+    BrowserModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/tic/tic.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TicRoutingModule } from './tic-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TicRoutingModule
+  ]
+})
+export class TicModule { }
